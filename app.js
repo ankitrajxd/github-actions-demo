9a0c3affa0650fa8166d37c8287545ff2c1d49e2
Ankit <ankitrajxd@gmail.com>
2024-10-11 23:35:17 +0530

    chore: Update greeting message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Updated Hello, World! from container!❤️');
+    res.send('Updated Hello, World! from pod!❤️');
 });
 
 app.listen(3000, () => {
